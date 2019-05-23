384dba4ab3f468e559c8cb8cdaa67f048c80506f
Ruihua Sui <rsui2@illinois.edu>
2019-05-23 17:54:24 -0500

    Updates

---
+++ precache-manifest.0326defa5451c37b9f217cd2a94f05a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d73eb3e66f1febdfdbbf1a25dae60a40",
+    "url": "/react-simple2048/index.html"
+  },
+  {
+    "revision": "0658367e536c645b9d09",
+    "url": "/react-simple2048/static/css/main.5a3cf8a3.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/react-simple2048/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "0658367e536c645b9d09",
+    "url": "/react-simple2048/static/js/main.2eb865c8.chunk.js"
+  },
+  {
+    "revision": "da81ef067d48cd22ceb4",
+    "url": "/react-simple2048/static/js/runtime~main.9bdcadfa.js"
+  }
+]);
